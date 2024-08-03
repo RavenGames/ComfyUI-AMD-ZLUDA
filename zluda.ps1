a612104f019429f553611598fcacc2fe61ca3a4d
RavenGames <11320046+RavenGames@users.noreply.github.com>
2024-08-04 00:33:53 +0200

    Update zluda.ps1

--- zluda.ps1
+++ zluda.ps1
@@ -32,13 +32,14 @@ def cuda_malloc_supported():
 #We don't need malloc at all with amd gpu's. So disabling all together
 "@
 
+$patched = $false
+
 if ((Get-Content -Raw -Path $filepath) -match [regex]::Escape($old_function)) {
     (Get-Content -Raw -Path $filepath) -replace [regex]::Escape($old_function), $new_function | Set-Content -Path $filepath
     Write-Host "cuda_malloc.py patched successfully."
-
+    $patched = $true
 } else {
     Write-Host "cuda_malloc.py already patched or pattern not found. Skipping..."
-
 }
 
 
@@ -106,12 +107,16 @@ except:
     print("Could not pick default device.")
 "@
 
-    if ((Get-Content -Raw -Path $filepath2) -match [regex]::Escape($old_function2)) {
+if ((Get-Content -Raw -Path $filepath2) -match [regex]::Escape($old_function2)) {
     (Get-Content -Raw -Path $filepath2) -replace [regex]::Escape($old_function2), $new_function2 | Set-Content -Path $filepath2
     Write-Host "model_management.py patched successfully."
+    $patched = $true
 } else {
     Write-Host "model_management.py already patched or pattern not found. Skipping..."
-
 }
 
-Read-Host -Prompt "ZLUDA is patched, press ENTER to close this window, now run ComfyUI via start.bat"
+if ($patched) {
+    Read-Host -Prompt "ZLUDA is patched, press ENTER to close this window, now run ComfyUI via start.bat"
+} else {
+    Read-Host -Prompt "Press ENTER to close this window, now run ComfyUI via start.bat"
+}
